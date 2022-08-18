0a8ac906a7a299a541e1eec4f44700382e62e8b3
Haggernaut <111538901+Hagernaut@users.noreply.github.com>
2022-08-19 00:47:51 +0200

    Runclass.java Documentation

--- src/main/RunClass.java
+++ src/main/RunClass.java
@@ -1,9 +1,16 @@
 package main;
 
 import frames.InitFrame;
-/**This is the main class of the Java applicattion.
+/**This is the main class of the Java applicattion. Obviously, this class contains
+ * the public static void main(String[] args) method that permits the application run.
+ * The only thig that this method do is calling to the class {@link InitFrame} to create
+ * the first frame of the program, the logo and the progressbar. 
+ * 
+ * Note that the frame created by {@link InitFrame} is linked to the rest of frames that
+ * are present in the entire program. 
  * @author Filipondios
- * @version 10.08.2022*/
+ * @see InitFrame
+ * @version 19.08.2022*/
 public class RunClass {
 	public static void main(String[] args) {			
 		@SuppressWarnings("unused")
