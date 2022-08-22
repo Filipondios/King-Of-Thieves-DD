f00e4b4031e7dc5ae4afcb445a7d2490dc5f9313
Filipondios <danialhamilla262@gmail.com>
2022-08-22 16:35:31 +0200

    Added the restart and exit the program

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -5,6 +5,11 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -13,6 +18,8 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import main.RunClass;
 
 /**This will be the main Frame for the application. Called by {@link InitFrame}, this window
  * sets an area where the user can open different dungeons and place traps and other stuff into
@@ -43,17 +50,65 @@ public class MainFrame extends JFrame{
 		setIconImage(new ImageIcon("resources/images/basic/icon.png").getImage());
 		
 		/* Create a menu bar for the main application */
-		JMenu index1 = new JMenu(); index1.setText("Dungeon");		
+		JMenu index1 = new JMenu(); index1.setText("Options");		
 		JMenu index2 = new JMenu(); index2.setText("Dungeon Default Stuff");
 		JMenu index3 = new JMenu(); index3.setText("Traps");
 		
-		JMenuItem item = new JMenuItem("Create/Open new Dungeon");
-		index1.add(item);
+		JMenuItem item1s1 = new JMenuItem("Create/Open new Dungeon");
+		JMenuItem item1s2 = new JMenuItem("Restart");
+		JMenuItem item1s3 = new JMenuItem("Exit");
+		index1.add(item1s1);
+		index1.add(item1s2);
+		index1.add(item1s3);
 		
-		item.addActionListener(new ActionListener() {
+		item1s1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dungeonFrame.setVisible(true);
+			}
+		});
+		
+		item1s2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+				File jarDir = null;
+				
+				try {
+					jarDir = new File(ClassLoader.getSystemClassLoader().getResource(".").toURI());
+				} catch (URISyntaxException e2) {
+					e2.printStackTrace();
+				}
+				
+				String[] dir = jarDir.list();
+				String jarFile = "";
+				
+				for (String obj : dir) {
+					if (obj.contains(".jar")) {
+						jarFile = obj; break;
+					}
+				}
+						
+				final ArrayList<String> command = new ArrayList<String>();
+				command.add(javaBin);
+				command.add("-jar");
+				command.add(jarDir+ File.separator +jarFile);
+
+				final ProcessBuilder builder = new ProcessBuilder(command);
+				try {
+					builder.start();
+				} catch (IOException e1) {
+					System.out.println("Start failed");
+					e1.printStackTrace();
+				}
+				System.exit(0);	
+			}
+		});
+		
+		item1s3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
 			}
 		});
 		
