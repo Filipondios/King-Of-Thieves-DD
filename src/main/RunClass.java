ec4778b0609036de6ea16e96e331722baa582679
Filipondios <danialhamilla262@gmail.com>
2022-09-05 15:17:47 +0200

    Update gittignore

--- src/main/RunClass.java
+++ src/main/RunClass.java
@@ -1,11 +1,11 @@
 package main;
 
 import frames.InitFrame;
-/**This is the main class of the Java applicattion. Obviously, this class contains
+/**This is the main class of the Java application. Obviously, this class contains
  * the public static void main(String[] args) method that permits the application run.
- * The only thig that this method do is calling to the class {@link InitFrame} to create
+ * The only thing that this method do is calling to the class {@link InitFrame} to create
  * the first frame of the program, the logo and the progressbar. 
- * 
+ * <br>
  * Note that the frame created by {@link InitFrame} is linked to the rest of frames that
  * are present in the entire program. 
  * @author Filipondios, Hagernaut
