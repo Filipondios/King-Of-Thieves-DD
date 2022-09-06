bf598a7b2488887c4300f483a323fb8546bd16a2
Filipondios <danialhamilla262@gmail.com>
2022-09-06 10:59:04 +0200

    Init frame updated

--- src/frames/InitFrame.java
+++ src/frames/InitFrame.java
@@ -5,13 +5,14 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
+import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import com.formdev.flatlaf.FlatDarkLaf;
+import FilipondiosUtils.ResourceLoader;
 
 /**Class that shows the first, and it's called by {@link MainFrame}, with a image from {@link ImagePanel} and
  * a progress bar from {@link JProgressBar}. Then it starts the second frame, from the class {@link MainFrame}
@@ -22,18 +23,18 @@ import com.formdev.flatlaf.FlatDarkLaf;
  * starts slower).
  * @author Filipondios, Hagernaut
  * @see MainFrame
- * @version 19.08.2022*/
+ * @version 05.09.2022*/
 public class InitFrame extends javax.swing.JFrame {
-	
-	ImagePanel ip = new ImagePanel();
+
 	private javax.swing.JProgressBar jProgressBar1;
 	private final javax.swing.Timer timer;
 	private int x = 0;
-	
+
 	/**Method that sets the basic configurations to the frame and make the instances for 
-	 * the {@link JProgressBar} and the image from {@link ImagePanel}.**/
-	public InitFrame() {
-		this.setContentPane(ip);
+	 * the {@link JProgressBar} and the image from {@link ImagePanel}.
+	 * @throws IOException **/
+	public InitFrame() throws IOException {
+		this.setContentPane(new ImagePanel());
 		initComponents();
 		setLocationRelativeTo(null);
 
@@ -72,48 +73,52 @@ public class InitFrame extends javax.swing.JFrame {
 		timer = new Timer(1, al);		
 		timer.start();
 	}
-	
+
 	/**Method that sets more basic configurations to the Frame and sets the values for the 
-	 * positions of the {@link JProgressBar} and the rest of the pane.**/
-	private void initComponents() {
+	 * positions of the {@link JProgressBar} and the rest of the pane.
+	 * @throws IOException **/
+	private void initComponents() throws IOException {
 		/* Basic Frame config */
 		setUndecorated(true);
 		setVisible(true);
-		setIconImage(new ImageIcon("resources/images/basic/icon.png").getImage());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
+		ResourceLoader loader = new ResourceLoader();
+		Image icon = loader.loadImage("/images/basic/icon.png");
+		setIconImage(icon);
+
 		/* General settings for the bar */
 		jProgressBar1 = new javax.swing.JProgressBar();
 		jProgressBar1.setBackground(new Color(132, 132, 132));
 		jProgressBar1.setForeground(new Color(25, 25, 25));
 		jProgressBar1.setBorder(new EmptyBorder(2,5,2,5));
-		    	
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jProgressBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 270, Short.MAX_VALUE)
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        pack();
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(
+				layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addComponent(jProgressBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
+				);
+		layout.setVerticalGroup(
+				layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+						.addGap(0, 270, Short.MAX_VALUE)
+						.addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE))
+				);
+		pack();
 	}
 }
 
 /**A simple class that makes visible a custom image in a {@link JPanel}.
  * @author Filipondios
- * @version 15.08.2022*/
+ * @version 05.09.2022*/
 class ImagePanel extends JPanel{
 
 	/**Method that overrides the method paint from {@link JPanel} and draws an image.*/
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		Image image = new ImageIcon("resources/images/init/logo.png").getImage();
-		g.drawImage(image,0,0,getWidth(),getHeight(),null);
+		ResourceLoader loader = new ResourceLoader();
+		g.drawImage(loader.loadImage("/images/init/logo.png"),0,0,getWidth(),getHeight(),null);
 	}
 }
 
