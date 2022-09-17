3a87afce75528c7c76e312e99e9e988f889ce61c
Filipondios <danialhamilla262@gmail.com>
2022-09-17 02:09:30 +0200

    Fix some bugs at Main Frame

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -5,68 +5,51 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import javax.swing.*;
-
-import FilipondiosUtils.ResourceLoader;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import FilipondiosUtils.WindowUtils;
 
 /**This will be the main Frame for the application. Called by {@link InitFrame}, this window
  * sets an area where the user can open different dungeons and place traps and other stuff into
  * the workspace created in this Frame.
  * @author Filipondios, Hagernaut 
- * @version 24.08.2022**/
+ * @version 17.09.2022**/
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame implements KeyListener{
 
-	private final JLayeredPane lpane = new JLayeredPane();
-	private int actualDungeon = 4;
-	protected static JPanel dungeonPane = new JPanel();
-	protected static JPanel gridPane = new JPanel();
-	JMenuBar menu = new JMenuBar();
+	public static final boolean[] board = new boolean[112];
 
 	/**Method that starts all the configurations for the Frame. **/
 	public MainFrame() {
-		/*Get the user's screen resolution, so the window will have for the first
-		 * time the dimensions: screenWidth/2 x screenHeight/2 */
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		Dimension screen  = tk.getScreenSize();
-		setSize(screen.width/2,screen.height/2);
-
-		/* Common Frame settings and other stuff */
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setResizable(false);
-		setTitle("King of Thieves Dungeon Designer");
-		setVisible(true);
 		setIconImage(new ImageIcon("resources/images/basic/icon.gif").getImage());
-
-		/* Frame Layout config. Creates to Panes in the frame: 
-		 * - The one with a grid image that is above the other one.
-		 * - A pane with the dungeon scheme, wich the user can change. */
-		add(lpane, BorderLayout.CENTER);
-		lpane.setBounds(0, 0, getWidth(), getHeight());
-
-		dungeonPane.setBounds(0, 0, getWidth(), getHeight());
-		dungeonPane.setOpaque(true);
-
-		gridPane.setBounds(0, 0, getWidth(), getHeight());
-		gridPane.setOpaque(false);
-
-		/* Sets the grid image */
-		ImageIcon grid = new ImageIcon(new ImageIcon("resources/images/bases/grid.gif").getImage()
-				.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH));
-		JLabel gridLabel = new JLabel(grid);
-		gridPane.add(gridLabel);
-		
-		/* Sets the dungeon no.4 as the background of the frame.*/
-		setDungeon(4);
-
-		/* Sets both panes one above the other */
-		lpane.add(dungeonPane, 0, 0);
-		lpane.add(gridPane, 1, 0);
-
+		this.setTitle("King of Thieves Dungeon Dessigner");
+		this.setLayout(new BorderLayout());
+		this.setSize(resizeWindowToGrid(WindowUtils.screenHeight/2));
+		this.setLocationRelativeTo(null);
+		this.setJMenuBar(createMenubar());
+		this.add(new BoardPanel(), BorderLayout.CENTER);
+		this.setResizable(false);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addKeyListener(this);
+	}
+	
+	private Dimension resizeWindowToGrid(final int height) {
+		return new Dimension(height/8*14,height);
+	}
+	
+	private JMenuBar createMenubar() {
 		/* Create a menu bar for the main application */
 		/* Add items to the menu-bar */
 		JMenu index1 = new JMenu(); index1.setText("Options");
@@ -74,82 +57,19 @@ public class MainFrame extends JFrame implements KeyListener{
 		JMenu index3 = new JMenu(); index3.setText("Traps");
 
 		/* Add sub-items to the first item of the menu-bar */
-		JMenuItem item1s1 = new JMenuItem("Restart Application");
-		JMenuItem item1s2 = new JMenuItem("Exit");
+		JMenuItem item1s1 = new JMenuItem("Save dungeon as...");
+		JMenuItem item1s2 = new JMenuItem("Restart Application");
+		JMenuItem item1s3 = new JMenuItem("Exit");
 		index1.add(item1s1);
 		index1.add(item1s2);
-		
-		/* Add sub-items to the second item of the menu-bar */
-		JMenuItem item2s1 = new JMenuItem("Totem");
-		JMenuItem item2s2 = new JMenuItem("Door");
-		JMenuItem item2s3 = new JMenuItem("Gravity Switch");
-		JMenuItem item2s4 = new JMenuItem("Platform");
-		JMenuItem item2s5 = new JMenuItem("Trampoline");
-		
-		item2s1.setIcon(new ImageIcon(new ImageIcon("resources/images/items/totem.gif").getImage()));
-		item2s2.setIcon(new ImageIcon(new ImageIcon("resources/images/items/door.gif").getImage()));
-		item2s3.setIcon(new ImageIcon(new ImageIcon("resources/images/items/gravity.gif").getImage()));
-		item2s4.setIcon(new ImageIcon(new ImageIcon("resources/images/items/platform.gif").getImage()));
-		item2s5.setIcon(new ImageIcon(new ImageIcon("resources/images/items/trampoline.gif").getImage()));
-		
-		index2.add(item2s1);
-		index2.add(item2s2);
-		index2.add(item2s3);
-		index2.add(item2s4);
-		index2.add(item2s5);
-
-		/* Add sub-items to the second item of the menu-bar */
-		JMenuItem item3s1 = new JMenuItem("Saw");
-		JMenuItem item3s2 = new JMenuItem("Red Guard");
-		JMenuItem item3s3 = new JMenuItem("Cannon");
-		JMenuItem item3s4 = new JMenuItem("Seeker Bird");
-		JMenuItem item3s5 = new JMenuItem("Homing Cannon");
-		JMenuItem item3s6 = new JMenuItem("Spinner");
-		JMenuItem item3s7 = new JMenuItem("Ricochet");
-		JMenuItem item3s8 = new JMenuItem("Lil' Scorcher");
-		JMenuItem item3s9 = new JMenuItem("Warder");
-		JMenuItem item3s10 = new JMenuItem("Roaster");
-		JMenuItem item3s11 = new JMenuItem("Bloodhound");
-		//JMenuItem item3s12 = new JMenuItem("Electro Cannon");
-		
-		
-		item3s1.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/saw.gif").getImage()));
-		item3s2.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/rg.gif").getImage()));
-		item3s3.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/cannon.gif").getImage()));
-		item3s4.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/fly.gif").getImage()));
-		item3s5.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/hcannon.gif").getImage()));
-		item3s6.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/spinner.gif").getImage()));
-		item3s7.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/ricochet.gif").getImage()));
-		item3s8.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/dragon.gif").getImage()));
-		item3s9.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/warder.gif").getImage()));
-		item3s10.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/roaster.gif").getImage()));
-		item3s11.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/bh.gif").getImage()));
-		//item3s5.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/.gif").getImage()));
-		
-		index3.add(item3s1);
-		index3.add(item3s2);
-		index3.add(item3s3);
-		index3.add(item3s4);
-		index3.add(item3s5);
-		index3.add(item3s6);
-		index3.add(item3s7);
-		index3.add(item3s8);
-		index3.add(item3s9);
-		index3.add(item3s10);
-		index3.add(item3s11);
-		
-		/* Adds all the menu-bar items and the menu-bar to the frame*/
-		menu.add(index1);
-		menu.add(index2);
-		menu.add(index3);
-		setJMenuBar(menu);
+		index1.add(item1s3);
 		
 		/* Adds a listener to the second sub-item of the first item of the menu-bar
 		 * (Restart). It restarts the current program. To do so, we search the binary file
 		 * of Java. In Windows, it should be like C:\Program Files\Java\jre1.8.0_161\bin\java,
 		 * and in Linux should be like /usr/bin/java. Then we see the current path of the running
 		 * .jar file, and then it runs this file and closes the app. */
-		item1s1.addActionListener(new ActionListener() {
+		item1s2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
@@ -196,31 +116,76 @@ public class MainFrame extends JFrame implements KeyListener{
 
 		/* Adds a listener to the third sub-item of the first item of the menu-bar
 		 * (Exit). It finishes the current program. */
-		item1s2.addActionListener(new ActionListener() {
+		item1s3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
-		});
+		});			
 		
-		/* Sets as keylistener this frame. This means that every key pressed
-		 * by the user with the frame active, will be processed if necessary.*/
-		addKeyListener(this);
-	}
+		/* Add sub-items to the second item of the menu-bar */
+		JMenuItem item2s1 = new JMenuItem("Totem");
+		JMenuItem item2s2 = new JMenuItem("Door");
+		JMenuItem item2s3 = new JMenuItem("Gravity Switch");
+		JMenuItem item2s4 = new JMenuItem("Platform");
+		JMenuItem item2s5 = new JMenuItem("Trampoline");
+		
+		item2s1.setIcon(new ImageIcon(new ImageIcon("resources/images/items/totem.gif").getImage()));
+		item2s2.setIcon(new ImageIcon(new ImageIcon("resources/images/items/door.gif").getImage()));
+		item2s3.setIcon(new ImageIcon(new ImageIcon("resources/images/items/gravity.gif").getImage()));
+		item2s4.setIcon(new ImageIcon(new ImageIcon("resources/images/items/platform.gif").getImage()));
+		item2s5.setIcon(new ImageIcon(new ImageIcon("resources/images/items/trampoline.gif").getImage()));
+		
+		index2.add(item2s1);
+		index2.add(item2s2);
+		index2.add(item2s3);
+		index2.add(item2s4);
+		index2.add(item2s5);
 
-	/**Method that sets to the background pane of the main frame an image that represents 
-	 * the scheme of a dungeon. If the pane is already filled by other image, it removes this one 
-	 * and adds the desired image.
-	 * @author Filipondios*/
-	private void setDungeon(int dungeonNumber){		
-		ImageIcon dungeon2 = new ImageIcon(new ImageIcon("resources/images/bases/"+dungeonNumber+".gif").getImage().
-				getScaledInstance(dungeonPane.getWidth(), dungeonPane.getHeight(), Image.SCALE_SMOOTH));
-
-		if (dungeonPane.getComponents().length!=0)
-			dungeonPane.remove(0);
-
-		JLabel dungeonLabel2 = new JLabel(dungeon2);
-		dungeonPane.add(dungeonLabel2);
+		/* Add sub-items to the second item of the menu-bar */
+		JMenuItem item3s1 = new JMenuItem("Saw");
+		JMenuItem item3s2 = new JMenuItem("Red Guard");
+		JMenuItem item3s3 = new JMenuItem("Cannon");
+		JMenuItem item3s4 = new JMenuItem("Seeker Bird");
+		JMenuItem item3s5 = new JMenuItem("Homing Cannon");
+		JMenuItem item3s6 = new JMenuItem("Spinner");
+		JMenuItem item3s7 = new JMenuItem("Ricochet");
+		JMenuItem item3s8 = new JMenuItem("Lil' Scorcher");
+		JMenuItem item3s9 = new JMenuItem("Warder");
+		JMenuItem item3s10 = new JMenuItem("Roaster");
+		JMenuItem item3s11 = new JMenuItem("Bloodhound");
+		
+		
+		item3s1.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/saw.gif").getImage()));
+		item3s2.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/rg.gif").getImage()));
+		item3s3.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/cannon.gif").getImage()));
+		item3s4.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/fly.gif").getImage()));
+		item3s5.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/hcannon.gif").getImage()));
+		item3s6.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/spinner.gif").getImage()));
+		item3s7.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/ricochet.gif").getImage()));
+		item3s8.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/dragon.gif").getImage()));
+		item3s9.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/warder.gif").getImage()));
+		item3s10.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/roaster.gif").getImage()));
+		item3s11.setIcon(new ImageIcon(new ImageIcon("resources/images/traps/bh.gif").getImage()));
+		
+		index3.add(item3s1);
+		index3.add(item3s2);
+		index3.add(item3s3);
+		index3.add(item3s4);
+		index3.add(item3s5);
+		index3.add(item3s6);
+		index3.add(item3s7);
+		index3.add(item3s8);
+		index3.add(item3s9);
+		index3.add(item3s10);
+		index3.add(item3s11);
+		
+		/* Adds all the menu-bar items and the menu-bar to the frame*/
+		JMenuBar menu = new JMenuBar();
+		menu.add(index1);
+		menu.add(index2);
+		menu.add(index3);
+		return menu;
 	}
 	
 	/**Method that any number that represents a dungeon represents a real dungeon.
@@ -229,39 +194,50 @@ public class MainFrame extends JFrame implements KeyListener{
 	private boolean isDungeon(int dungeonNumber) {
 		int[] notDungeons = {50,51,55,59,60,85,86};
 		for (int e : notDungeons)
-			if (dungeonNumber==e) { return false; }
+			if (dungeonNumber==e)
+				return false;
 		return true;
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) { 
-		/* Pressing the Right Key, the dungeon changes to the next*/
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT && actualDungeon<125) {
-			if (isDungeon(actualDungeon+1)) {
-				actualDungeon++;
-			}else {
-				while (!isDungeon(actualDungeon+1)||!isDungeon(actualDungeon))
-					actualDungeon++;
-			}
-		}
-		
-		/* Pressing the Right Key, the dungeon changes to the next*/
-		if(e.getKeyCode() == KeyEvent.VK_LEFT && actualDungeon>4) {
-			if (isDungeon(actualDungeon-1)) {
-				actualDungeon--;
-			}else {
-				while (!isDungeon(actualDungeon-1)||!isDungeon(actualDungeon))
-					actualDungeon--;
-			}
-		}
-		
-		this.setTitle("King of Thieves Dungeon Designer (Base "+actualDungeon+")");
-		setDungeon(actualDungeon);
-	}
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {}
+	@Override
+	public void keyPressed(KeyEvent e) {}
 	
-	@Override
-	public void keyTyped(KeyEvent e) { }
+private final class BoardPanel extends JPanel{
+		
+		public BoardPanel() {
+			super(new GridLayout(8,14));
+			for (int i = 0; i < 112; i++)
+				add(new TilePanel(this,i));				
+		}
+}
 
-	@Override
-	public void keyReleased(KeyEvent e) { }
+	private final class TilePanel extends JPanel implements MouseListener{
+		private final int tileId;
+
+		public TilePanel(final BoardPanel boardPanel, final int tileId) {
+			this.setLayout(new GridBagLayout());
+			this.tileId = tileId;
+			assignColor(tileId);
+			addMouseListener(this);
+		}
+
+		private void assignColor(final int tileId) {
+			setBackground(Color.white);
+			setBorder(new LineBorder(Color.BLACK));
+			MainFrame.board[tileId]=true;
+		}
+
+		public void mouseClicked(MouseEvent e) {
+			setBackground(Color.black);
+		}
+
+		public void mousePressed(MouseEvent e) {/*Nothing TODO*/}
+		public void mouseReleased(MouseEvent e) {/*Nothing TODO*/}
+		public void mouseEntered(MouseEvent e) {/*Nothing TODO*/}
+		public void mouseExited(MouseEvent e) {/*Nothing TODO*/}
+	}
 }
