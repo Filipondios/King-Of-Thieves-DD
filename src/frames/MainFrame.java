36f273e05da7588d7ccfa6f51652d1672d467282
Perez Valverde Daniel <danialhamilla262@gmail.com>
2022-10-07 18:30:59 +0200

    Update MainFrame

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -1,45 +1,37 @@
 package frames;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Iterator;
-
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
+import javax.swing.JOptionPane;
 
-import filipondiosUtils.Pair;
+import main.RunClass;
 import traps.*;
 
-/**This will be the main Frame for the application. Called by {@link InitFrame}, this window
+/**This will be the main Frame for the application. Called by {@link RunClass}, this window
  * sets an area where the user can open different dungeons and place traps and other stuff into
  * the workspace created in this Frame.
  * @author Filipondios, Hagernaut 
- * @version 03.10.2022**/
+ * @version 06.10.2022**/
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame implements KeyListener{
 
-	public static Integer[][] BOARD_COMPOSITION;
-	BoardPanel board = new BoardPanel();
+	protected static ArrayList<Integer> BOARD_COMPOSITION = new ArrayList<>(160);
+	private BoardPanel board = new BoardPanel();
 
 	/**Method that starts all the configurations for the Frame. **/
 	public MainFrame() {
@@ -53,18 +45,17 @@ public class MainFrame extends JFrame implements KeyListener{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.addKeyListener(this);
 		this.setVisible(true);
-		importDungeon("resources/data/binBases/4.bs");
 	}
 
+	/** Method that creates a menubar for the application and adds it to the frame.*/
 	private JMenuBar createMenubar() {
-		/* Create a menu bar for the main application */
 		/* Add items to the menu-bar */
 		JMenu index1 = new JMenu(); index1.setText("Options");
 		JMenu index2 = new JMenu(); index2.setText("Dungeon Default Stuff");
 		JMenu index3 = new JMenu(); index3.setText("Traps");
 
 		/* Add sub-items to the first item of the menu-bar */
-		JMenuItem item1s1 = new JMenuItem("Clear Dungeon and start from scratch");
+		JMenuItem item1s1 = new JMenuItem("Import dungeon (From ID number or name)");
 		JMenuItem item1s2 = new JMenuItem("Save dungeon as...");
 		JMenuItem item1s3 = new JMenuItem("Exit");
 		index1.add(item1s1);
@@ -130,18 +121,22 @@ public class MainFrame extends JFrame implements KeyListener{
 		menu.add(index2);
 		menu.add(index3);
 	
+		/* Add listeners to the menu-bar items and sub-items */
 		item1s1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) { 
-				importDungeon("resources/data/binBases/blank.bs");
-				board.setLayout(BOARD_COMPOSITION);
+				String str = JOptionPane.showInputDialog(null, "Type the base number/name to import.\n"
+						+ "NOTE: If you want an empty dungeon, type 0.");
+				importDungeon("resources/data/binBases/"+str+".bs");
+				board.setScheme(BOARD_COMPOSITION);
 			}
 		});	
 		
 		item1s2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) { 
-				exportDungeon(BOARD_COMPOSITION);
+				String str = JOptionPane.showInputDialog(null, "Type the base number:");
+				if(str!=null) exportDungeon(BOARD_COMPOSITION,str);
 			}
 		});	
 		
@@ -155,114 +150,26 @@ public class MainFrame extends JFrame implements KeyListener{
 		return menu;
 	}
 
-
-	public void exportDungeon(Integer[][] dungeon) {
+	private void exportDungeon(Object dungeon, String iD) {
 		try {
-			FileOutputStream fos = new FileOutputStream("resources/data/binBases/blank.bs");
-			ObjectOutputStream ous = new ObjectOutputStream(fos);
+			ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream("resources/data/binBases/"+iD+".bs"));
 			ous.writeObject(dungeon);
-			fos.close();
+			ous.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void importDungeon(String file) {
+	private void importDungeon(String path) {
 		try {
-			FileInputStream fis = new FileInputStream(file);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			BOARD_COMPOSITION = (Integer[][]) ois.readObject();
-			
-			fis.close();
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+			BOARD_COMPOSITION = (ArrayList<Integer>) ois.readObject();
+			ois.close();
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-
-
-
-
-
-
-
-
-	/**Method that checks if any number that represents a real dungeon.
-	 * @author Filipondios
-	 * @return True if the number represents a real dungeon, false if dont.*/
-	/*private boolean isDungeon(int dungeonNumber) {
-		int[] notDungeons = {50,51,55,59,60,85,86};
-		for (int e : notDungeons)
-			if (dungeonNumber==e)
-				return false;
-		return true;
-	}*/
-
-	private final class BoardPanel extends JPanel{
-		
-		private TilePanel[] tiles = new TilePanel[160];
-		
-		public BoardPanel() {
-			super(new GridLayout(10,16));
-			for (int i = 0; i < 160; i++) {
-				tiles[i] = new TilePanel(this,i);
-				this.add(tiles[i]);
-			}
-		}
-		
-		public void setLayout(Integer[][] layout) {
-			for (int i = 0; i < layout.length; i++) {
-				for (int j = 0; j < layout[0].length; j++) {
-					if(layout[i][j]==1) tiles[i*j].setBackground(Color.black);
-					else tiles[i*j].setBackground(Color.white);
-				}
-			}
-		}
-	}
-
-	private final class TilePanel extends JPanel {
-		
-		private static Pair<Integer, Integer> cc = new Pair<Integer,Integer>(0,0);
-		private Pair<Integer, Integer> coordinate;
- 		
-		public TilePanel(BoardPanel panel, int tileID) {
-			coordinate= new Pair<Integer, Integer>(cc.getKey(), cc.getValue());
-			this.setBackground(assignColor());
-			checkPositions();
-			this.setBorder(new LineBorder(Color.BLACK));
-		}
-		
-		private Color assignColor() {
-			return (cc.getKey()==0 || cc.getKey() ==9 
-					|| cc.getValue()==0 || cc.getValue()==15)? Color.black : Color.white;
-		}
-		
-		private void checkPositions() {
-			if(cc.getValue()==15) {
-				cc.setValue(0);
-				cc.setKey(cc.getKey()+1);
-			} else cc.setValue(cc.getValue()+1);
-		}
-	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public void keyTyped(KeyEvent e) {/*Nothing TODO*/}
 	public void keyReleased(KeyEvent e) {/*Nothing TODO*/}
 	public void keyPressed(KeyEvent e) {/*Nothing TODO*/}
