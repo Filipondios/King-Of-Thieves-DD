e33cf1e77a873ae6a06b488c5f8e85d1051d1d41
Filipondios <danialhamilla262@gmail.com>
2022-10-20 23:03:48 +0200

    move to utils package and update

---
+++ src/app/kotdd/utils/DragableLabel.java
@@ -0,0 +1,20 @@
+package app.kotdd.utils;
+
+import java.awt.Cursor;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class DragableLabel extends JLabel {
+	
+	public DragableLabel() {		
+		MouseAdapter mouseAdapter = new MouseAdapter() {
+			public void mouseDragged(MouseEvent e){
+				setLocation(getX()+e.getX()-getWidth()/2,getY()+e.getY()-getHeight()/2);
+			}
+		};
+		this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		this.addMouseMotionListener(mouseAdapter);
+	}
+}
