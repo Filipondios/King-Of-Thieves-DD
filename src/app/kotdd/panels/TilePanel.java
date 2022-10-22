1300d53c946bd3a0dce9edd3ddf4e8f34c981d4a
MrHagg <111538901+Hagernaut@users.noreply.github.com>
2022-10-22 14:42:53 +0200

    TilePanel documentation

--- src/app/kotdd/panels/TilePanel.java
+++ src/app/kotdd/panels/TilePanel.java
@@ -5,16 +5,26 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
-
 import app.kotdd.frames.MainFrame;
 import app.kotdd.utils.Pair;
 
 @SuppressWarnings("serial")
+/**Class that extends fom {@link JPanel} and represents a dungeon block.
+ * @author Filipondios, Haggernaut*/
 public final class TilePanel extends JPanel implements MouseListener{
 	
+	/** Static variable that counts the rows and columns of the actual block in the dungeon. 
+	* When a {@link LayoutPanel} is created, 160 {@TilePanel}s are also created, and added
+	* to itself. A proper way to control whether the block is part of the margin or the dungeon,
+	* is controlling if the actual block is in a row or a column that is part of a margin of the dungeon.
+	* Rows are storaged in cc.key and columns in cc.value */
 	private static Pair<Integer, Integer> cc = new Pair<Integer,Integer>(0,0);
-	private int tileID=0;
-		
+	/** Integer that represents the position of this block position in the {@link MainFrame} BOARD_COMPOSITION */
+	private int tileID = 0;
+
+	/** Main constructor. Creates a dungeon block by a integer that represents the position that this 
+	* block will occupy in the {@link MainFrame} BOARD_COMPOSITION 
+	* @param tileID Block id. */
 	public TilePanel(int tileID) {
 		this.tileID = tileID;
 		this.setBackground(assignColor());
@@ -23,12 +33,17 @@ public final class TilePanel extends JPanel implements MouseListener{
 		this.addMouseListener(this);
 	}
 	
+	/** Assign a color (white/black) depending on the position of this block in the {@link MainFrame}
+	* BOARD_COMPOSITION, by the static cc variable that controls the rows and columns of the dungeon.*/
 	private Color assignColor() {
 		Color color = (cc.getKey()==0 || cc.getKey() ==9 || cc.getValue()==0 || cc.getValue()==15)? Color.black : Color.white;
-		MainFrame.BOARD_COMPOSITION.add((color.equals(Color.black))? 1: 0);
+		MainFrame.BOARD_COMPOSITION.add((color.equals(Color.black))? 1 : 0);
 		return color;
 	}
 	
+	/** Method that sets the right rows and columns for the next block to be added to {@link LayoutPanel}.
+	* Because we add the blocks row by row in the {@link LayoutPanel}, we need to change if the column
+	* value is 15 (real row 16), to 0 and the columns to 0 too. Else, increase the column value. */
 	private void checkPositions() {
 		if(cc.getValue()==15) {
 			cc.setValue(0);
