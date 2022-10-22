696abdeca6463af0367fb03f1819d0a0f8cc90c1
MrHagg <111538901+Hagernaut@users.noreply.github.com>
2022-10-22 15:09:31 +0200

    Trap documentation

--- src/app/types/Trap.java
+++ src/app/types/Trap.java
@@ -1,16 +1,21 @@
 package app.types;
 
+/**Class that storages the data of a trap. This class extends from {@link Item} because both classes share the
+ * attributes id and dimension.
+ * @author Filipondios, Haggernaut*/
 public class Trap extends Item {
-
+	/** Boolean value that tells if the trap can move ( In the original King of Thieves game, 
+	* when the trap has a yellow bar with the dot in the editor mode). */
 	private boolean can_move;
+	/** Boolean value that tells if the trap can rotate ( In the original King of Thieves game, 
+	* when you click the trap and rotates in the editor mode). */
 	private boolean can_rotate;
 	
-	/*public Trap() {
-		super(null,null);
-		this.can_move = false;
-		this.can_rotate = false;
-	}*/
-	
+	/**Constructor using fields
+	 * @param can_move Boolean value that tells if the trap can move
+	 * @param rotable Boolean value that tells if the trap can rotate
+	 * @param id String that represents the id of a trap
+	 * @param dimension Integer array that contains the dimension of the trap*/
 	public Trap(boolean can_move, boolean rotable, String id, int[] dimension) {
 		super(id,dimension);
 		this.can_move = can_move;
