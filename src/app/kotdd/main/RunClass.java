84693e51e761436684eb406d8e7741db96d5cea2
Filipondios <danialhamilla262@gmail.com>
2022-10-23 13:33:46 +0200

    Update RunClass

--- src/app/kotdd/main/RunClass.java
+++ src/app/kotdd/main/RunClass.java
@@ -5,13 +5,13 @@ import app.kotdd.frames.MainFrame;
 
 /**This is the main class of the Java application. Obviously, this class contains
  * the public static void main(String[] args) method that permits the application run.
- * The only thing that this method do is calling the {@link InitFrame} class for create
+ * The only thing that this method do is calling the {@link MainFrame} class for create
  * the first frame of the program, with the editor panel. 
  * <br>
  * Note that the frame created by {@link MainFrame} is linked to the rest of frames that
  * are present in the entire program. 
  * @author Filipondios, Hagernaut
- * @see InitFrame
+ * @see MainFrame
  * @version 06.10.2022*/
 public class RunClass {
 	
@@ -21,8 +21,8 @@ public class RunClass {
 			new MainFrame();
 		} catch (Exception ex) {
 			/*Could do a multi try-catch with ClassNotFoundException, InstantiationException,
-			 * IllegalAccessException, UnsupportedLookAndFeelException but its cleaner to
-			 * do a single try-catch with the general Exception cause we know that the only thing
+			 * IllegalAccessException, UnsupportedLookAndFeelException, but it's cleaner to
+			 * do a single try-catch with the general Exception because we know that the only thing
 			 * we are specting is to get the Windows Theme. Otherwise, the theme will be the default (Metal).*/
 			ex.printStackTrace();
 		}
