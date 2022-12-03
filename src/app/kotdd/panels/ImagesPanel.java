2887f36a56f4200d1112d7c538f47b488135d109
dpv927 <dpv927@inlumine.ual.es>
2022-12-03 01:09:23 +0100

    Update Images Panel

--- src/app/kotdd/panels/ImagesPanel.java
+++ src/app/kotdd/panels/ImagesPanel.java
@@ -4,20 +4,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.JPanel;
-import app.kotdd.types.Item;
-import app.kotdd.types.Trap;
-import app.kotdd.utils.DraggableLabel;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
+import app.kotdd.miscellaneous.DraggableLabel;
+import app.kotdd.miscellaneous.Item;
+import app.kotdd.miscellaneous.Trap;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+/** Class that extends from JPanel. This panel is made for placing the different traps
+ * and items labels that the user puts in the frame.
+ * @author Filipondios 
+ * @version 03.12.2022*/
 public class ImagesPanel extends JPanel{
 	
 	public static ArrayList<Item> ITEMS = new ArrayList<>(5); // ArrayList with every item data: width, height
 	public static ArrayList<Trap> TRAPS = new ArrayList<>(12); // ArrayList with every item data: width, height, can rotate, etc.
 	
+	/** Creates a Images Panel. Then catches all the config data from the 
+	 * <code>resources/data/jsonAppConfig/</code> dir. The config is saved in those files
+	 * because thats the best way for the user to adjust the items and traps render
+	 * if there was a problem with that. */
 	public ImagesPanel() {
 		this.setSize(1050, 670);
 		this.setOpaque(false);
@@ -25,6 +33,10 @@ public class ImagesPanel extends JPanel{
 		importItemsPreferences();
 	}
 
+	/**
+	 * Adds a item to the panel.
+	 * @param id ID of the item to add.
+	 */
 	public void addItem(String id){
 		Item tmp = new Item(id,null);
 		for (Item item : ITEMS) {
@@ -37,6 +49,10 @@ public class ImagesPanel extends JPanel{
 		}
 	}
 
+	/**
+	 * Adds a trap to the panel.
+	 * @param id ID of the trap to add.
+	 */
 	public void addTrap(String id) {
 		Trap tmp = new Trap(false,false,id,null);
 		for (Trap trap : TRAPS) {
@@ -48,10 +64,12 @@ public class ImagesPanel extends JPanel{
 			}
 		}
 	}
+
+	/** Imports the traps config from the file <code>resources/data/jsonAppConfig/traps_config.json</code> */
 	private void importTrapsPreferences() {
 		try {
 			JSONParser parser = new JSONParser();
-			FileReader reader = new FileReader("resources/data/json/traps_config.json");
+			FileReader reader = new FileReader("resources/data/jsonAppConfig/traps_config.json");
 			Object obj = parser.parse(reader);
 			JSONArray traps = (JSONArray) obj;
 			
@@ -68,10 +86,11 @@ public class ImagesPanel extends JPanel{
 		}catch(IOException | ParseException e) { e.printStackTrace(); }
 	}
 	
+	/** Imports the items config from the file <code>resources/data/jsonAppConfig/items_config.json</code> */
 	private void importItemsPreferences() {
 		try {
 			JSONParser parser = new JSONParser();
-			FileReader reader = new FileReader("resources/data/json/items_config.json");
+			FileReader reader = new FileReader("resources/data/jsonAppConfig/items_config.json");
 			Object obj = parser.parse(reader);
 			JSONArray traps = (JSONArray) obj;
 			
